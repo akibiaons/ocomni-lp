7e357d99a09c320dc371271be076243cc45f4863
Arthur Fernandez <arthur@snooget.com>
2023-08-31 13:23:17 -0700

    created fixed navbar, and added headline + subheading

--- components/body.tsx
+++ components/body.tsx
@@ -2,14 +2,16 @@ import React from "react";
 
 export function Body() {
   return (
-    <div>
-      <div className="container">
-        <h1>Powering E-Commerce</h1>
+    <div className="pt-20">
+      <div className="container mx-auto">
+        <h1 className="text-2xl font-semibold pt-8">Powering E-Commerce</h1>
         <div>
           <p>
             Build a simple shop, no coing or design needed, and start growing
             today.
           </p>
+          <br></br>
+          <p className="pb-[2000px]">some</p>
         </div>
       </div>
     </div>
--- components/header.tsx
+++ components/header.tsx
@@ -3,8 +3,8 @@ import React from "react";
 export function Header() {
   return (
     <div>
-      <div className="uppercase font-extrabold text-[#5B45BB] cursor-pointer text-2xl sm:mt-1 lg:mr-6 text-center p-8 lg:text-left md:text-left">
-        Ocomni
+      <div className="uppercase fixed top-0 bottom-0 font-extrabold text-[#5B45BB] cursor-pointer text-2xl sm:mt-1 lg:mr-6 text-center p-8 lg:text-left md:text-left">
+        <p>Ocomni</p>
       </div>
     </div>
   );
