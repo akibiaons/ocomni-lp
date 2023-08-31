0e905c63f5038226bdc3f5f81b6df79539dfa24a
Arthur Fernandez <arthur@snooget.com>
2023-08-31 11:16:42 -0700

    Don't have users, deleted usless code

--- utils/users.js
+++
@@ -1,8 +0,0 @@
-import { firestore } from "firebase/firestore";
-
-const getUsers = async () => {
-  const snapshot = await firestore.collection("users").get();
-  snapshot.docs.forEach((docs = console.log(doc.data)));
-};
-
-export { getUsers };
