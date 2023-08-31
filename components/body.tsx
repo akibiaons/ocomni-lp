99ae0faf05d0a702c613b47338e6da579f43ab75
Arthur Fernandez <arthur@snooget.com>
2023-08-31 14:16:50 -0700

    Adding in carousel/animated lines

--- components/body.tsx
+++ components/body.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Carousel } from "./carousel";
 
 export function Body() {
   return (
@@ -10,8 +11,17 @@ export function Body() {
             Build a simple shop, no coing or design needed, and start growing
             today.
           </p>
-          <br></br>
-          <p className="pb-[2000px]">some</p>
+        </div>
+      </div>
+      <div className="pt-20 grid grid-cols-8 pl-8">
+        <div className="col-start-1 col-end-5 border-2 border-blue-600">
+          <div>
+            <img src="https://res.cloudinary.com/daf0osydi/image/upload/v1693514835/ocomni-lp-img_vcs14g.png" />
+          </div>
+        </div>
+        {/* Below will be the benefit driven content peices that pop up at the speed of the carousel cuts */}
+        <div className="col-start-5 col-end-9">
+          <Carousel />
         </div>
       </div>
     </div>
---
+++ components/carousel.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+
+export function Carousel() {
+  const img = ["", "", ""];
+
+  const [activeSlide, setActiveSlide] = useState(0);
+
+  useEffect(() => {
+    // Timer start here:
+    const timer = setInterval(() => {
+      setActiveSlide((prevSlide) => (prevSlide + 1) % img.length);
+    }, 4400);
+
+    // Gotta clear the timer below...first-letter...
+    return () => clearInterval(timer);
+  }, [img.length]);
+
+  return (
+    <>
+      <div className="relative flex flex-col items-center pb-4">
+        <div className="container text-left mb-10 mt-8 max-w-lg mx-auto px-4">
+          <img src={img[activeSlide]} className="w-full h-full" />
+        </div>
+        <div className="flex justify-center mt-4">
+          {img.map((_, index) => (
+            <div
+              key={index}
+              className={`h-2 w-2 mx-1 rounded-full ${
+                index === activeSlide ? "bg-gray-300" : "bg-white"
+              }`}
+            ></div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
