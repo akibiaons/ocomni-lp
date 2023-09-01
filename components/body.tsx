557cc15de419f849e26b9e663734fb1859683df8
Arthur Fernandez <arthur@snooget.com>
2023-08-31 21:01:49 -0700

    Added main content deleted old content

--- components/body.tsx
+++ components/body.tsx
@@ -1,30 +1,26 @@
 "use client";
 
-import React, { useState } from "react";
-import { Carousel } from "@/components/carousel";
-import { BenefitsCarousel } from "@/components/benefits-carousel";
-
 export function Body() {
   return (
-    <div className="pt-8">
-      <div className="container mx-auto text-center lg:pt-10 lg:pb-[12rem]">
-        <h1 className="text-2xl lg:text-6xl font-semibold">
-          Powering E-Commerce Growth
-        </h1>
-        <div>
-          <p className="mt-4 text-md font-extralight lg:text-2xl md:font-normal lg:font-normal">
-            Build a simple shop, no coding or design needed, and start growing
-            today.
+    <>
+      <div className="grid grid-cols-8">
+        <div className="col-start-2 col-end-5">
+          <h1>Powering E-Commerce with Ocomni</h1>
+          <p>
+            Streamlined, affordable, easy to use sites for ecommerce purposes
           </p>
-        </div>
-      </div>
-      <div className="lg:pt-10 lg:grid lg:grid-cols-8 lg:pl-8 p-8 pt-20">
-        <div className="lg:col-start-1 lg:col-end-5 border-2">
-          <div>
-            <Carousel />
+          <div className="">
+            <p className="mt-8">
+              <span className="border-2 rounded-md p-2 hover:cursor-pointer hover:bg-slate-200">
+                Learn More
+              </span>
+            </p>
           </div>
         </div>
+        <div className="col-start-5 col-end-8">
+          <img src="" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
