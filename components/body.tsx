fb4442fcddc227e0c1b9e831c1fe8fe52b1da19d
Arthur Fernandez <arthur@snooget.com>
2023-09-01 06:08:38 -0700

    completed hero section as needed

--- components/body.tsx
+++ components/body.tsx
@@ -4,20 +4,23 @@ export function Body() {
   return (
     <>
       <div className="grid grid-cols-8">
-        <div className="col-start-2 col-end-5">
-          <h1>Powering E-Commerce with Ocomni</h1>
-          <p>
+        <div className="col-start-2 col-end-5 pt-20 pr-2">
+          <h1 className="text-lg lg:text-5xl font-semibold pb-8 text-wrap">
+            Powering E-Commerce with
+            <span className="text-[#5B45BB]"> Ocomni</span>
+          </h1>
+          <p className="text-md lg:text-xl pb-8">
             Streamlined, affordable, easy to use sites for ecommerce purposes
           </p>
           <div className="">
             <p className="mt-8">
-              <span className="border-2 rounded-md p-2 hover:cursor-pointer hover:bg-slate-200">
-                Learn More
+              <span className="border-2 rounded-md p-2 hover:cursor-pointer bg-slate-100 hover:bg-slate-300">
+                Learn More & Turn BTN Purple
               </span>
             </p>
           </div>
         </div>
-        <div className="col-start-5 col-end-8">
+        <div className="col-start-5 col-end-8 pt-8">
           <div className="img-container">
             <div className="img-behind-wrapper">
               <img
--- components/header.tsx
+++ components/header.tsx
@@ -3,10 +3,15 @@ import React from "react";
 export function Header() {
   return (
     <div>
-      <div className="uppercase text-center lg:text-left md:text-left font-extrabold text-[#5B45BB] text-2xl sm:mt-1 lg:mr-6 p-8">
-        <div>
+      <div className="flex justify-around uppercase text-center lg:text-left md:text-left text-2xl sm:mt-1 lg:mr-6 p-8">
+        <div className="text-[#5B45BB] font-extrabold">
           <p>
             <span className="cursor-pointer">Ocomni</span>
+          </p>
+        </div>
+        <div className="border p-2 text-sm rounded-md bg-slate-100 hover:bg-slate-300 hover:cursor-pointer">
+          <p className="">
+            <span>Get Custom Sites Built Here 👋 </span>
           </p>
         </div>
       </div>
