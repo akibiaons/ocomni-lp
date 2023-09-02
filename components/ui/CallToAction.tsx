6fc01e6de2a910e794dd272863acf9adbe6e0fec
Arthur Fernandez <arthur@snooget.com>
2023-09-02 05:47:41 -0700

    html, css keyframes, and useEffect for screen portfolio

--- components/ui/CallToAction.tsx
+++ components/ui/CallToAction.tsx
@@ -1,8 +1,19 @@
 "use client";
 
 import React from "react";
+import { useState, useEffect } from "react";
 
 export function CallToAction() {
+  const [currentPortfolio, setCurrentPortfolio] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentPortfolio((prev) => (prev + 1) % 3); // cycle between 0, 1, 2
+    }, 5000); // change every 5 seconds, adjust as needed
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
       <div className="text-center max-w-[70%] container mx-auto">
@@ -13,22 +24,45 @@ export function CallToAction() {
             <span className="text-[#5B45BB]"> Ocomni</span>
           </h2>
         </div>
-        <div className="image-container">
-          <img
-            className="portfolio-one"
-            src="https://res.cloudinary.com/dv5fxnfot/image/upload/v1693610586/reshoot-glasses_mhtbxx.png"
-            alt=""
-          />
-          <img
-            className="portfolio-two"
-            src="https://res.cloudinary.com/dv5fxnfot/image/upload/v1693610585/cowboy-2-template_d6b0zj.png"
-            alt=""
-          />
-          <img
-            className="portfolio-three"
-            src="https://res.cloudinary.com/dv5fxnfot/image/upload/v1693610585/reshoot-flowers_yk3tb1.png"
-            alt=""
-          />
+        {/* Above is the CTA Text and below is the CTA Animation screen */}
+        <div className="flex space x-4">
+          <div className="laptop">
+            <img
+              src="https://res.cloudinary.com/dv5fxnfot/image/upload/v1693613251/macbookmockup_hh9dhw.png"
+              alt="Laptop Device"
+            />
+            <div className="screen-content">
+              <div
+                className={`screen-content ${
+                  currentPortfolio === 0 ? "fadeIn" : "fadeOut"
+                }`}
+              ></div>
+            </div>
+            <div
+              className={`screen-content ${
+                currentPortfolio === 1 ? "fadeIn" : "fadeOut"
+              }`}
+            >
+              {/* Portfolio 2 content */}
+            </div>
+
+            <div
+              className={`screen-content ${
+                currentPortfolio === 3 ? "fadeIn" : "fadeOut"
+              }`}
+            >
+              {/* Portfolio 3 content */}
+            </div>
+          </div>
+          <div className="phone">
+            <img
+              src="https://res.cloudinary.com/dv5fxnfot/image/upload/v1693613251/mobilephone_hvb50n.png"
+              alt="mobile device"
+            />
+            <div className="screen-content">
+              {/* Portfolio two items will go here... */}
+            </div>
+          </div>
         </div>
       </div>
     </>
