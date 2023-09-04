066e87329423f96e1e77773116cfa3bc8bd33b44
Arthur Fernandez <arthur@snooget.com>
2023-09-04 14:03:49 -0700

    header responsiveness

--- components/header.tsx
+++ components/header.tsx
@@ -3,15 +3,17 @@ import React from "react";
 export function Header() {
   return (
     <div>
-      <div className="flex justify-around uppercase text-center lg:text-left md:text-left text-2xl sm:mt-1 lg:mr-6 p-8">
-        <div className="text-[#5B45BB] font-extrabold">
+      <div className="lg:flex lg:justify-around lg:uppercase lg:text-left lg:text-2xl lg:mr-6 lg:p-8  md:flex md:justify-around md:uppercase md:text-left md:text-xl md:mr-6 md:p-8 text-center grid grid-cols-2 p-4">
+        <div className="text-[#5B45BB] lg:font-extrabold md:font-extrabold font-extrabold">
           <p>
-            <span className="cursor-pointer">Ocomni</span>
+            <span className="lg:cursor-pointer lg:pr-12 md:pr-12 pr-10">
+              Ocomni
+            </span>
           </p>
         </div>
         <div className="border p-2 text-sm rounded-md hover:cursor-pointer hover:bg-slate-300 bg-[#5B45BB] text-white">
           <p>
-            <span>Get Custom Sites Built Here 👋 </span>
+            <span>Custom Sites Here 👋</span>
           </p>
         </div>
       </div>
