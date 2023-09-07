f3aa19dd53438b3cbab93b49ac866df9cf0446c9
Arthur Fernandez <arthur@snooget.com>
2023-09-07 08:35:50 -0700

    changed up container for templates and arrs

--- app/(root)/signup/page.tsx
+++ app/(root)/signup/page.tsx
@@ -9,7 +9,7 @@ const SignUpPage = () => {
     "https://res.cloudinary.com/dv5fxnfot/image/upload/v1693851034/1_dnm1bk.svg",
   ];
 
-  const [currentTemplate, setCurrentTemplate] = useState(0);
+  const [currentImage, setCurrentImage] = useState(0);
 
   return (
     <>
@@ -20,13 +20,26 @@ const SignUpPage = () => {
           </span>
         </h1>
       </div>
-      <div className="grid grid-rows-2 border-2 border-red-500 mt-10 spacing-- lg:grid lg:grid-cols-12 lg-grid-rows-1 lg:border-2 lg:border-blue-500">
+      <div className="grid grid-rows-2 border-2 border-red-500 mt-10 lg:grid lg:grid-cols-12 lg-grid-rows-1 lg:border-2 lg:border-blue-500">
         <div className="text-center sign-up-form row-start-2 lg:row-start-1 lg:col-start-3 lg:col-end-6">
           <p>Sign Up</p>
         </div>
-        <div className="text-center carousel-signup lg:col-start-7 lg:col-end-12 lg:border-2 lg:border-green-500">
-          <p>Images</p>
-          {/*images*/}
+        <div
+          className="carousel-signup bg-cover bg-center h-96 lg:col-start-7 lg:col-end-12 lg:border-2 lg:border-green-500 lg:h-[40rem] lg:w-full"
+          style={{
+            backgroundImage: `url(${images[currentImage]})`,
+          }}
+        >
+          <div className="flex">
+            <div
+              className="selector-1 h-0 w-0 border-x-8 border-x-transparent border-b-[16px] border-b-black mr-8 hover:cursor-pointer hover:border-b-slate-500"
+              style={{ transform: "rotate(-90deg)" }}
+            />
+            <div
+              className="selector-2 h-0 w-0 border-x-8 border-x-transparent border-b-[16px] border-black hover:cursor-pointer hover:border-b-slate-500"
+              style={{ transform: "rotate(90deg)" }}
+            />
+          </div>
         </div>
       </div>
     </>
