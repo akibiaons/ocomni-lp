ec7af7594ed67bd5224b45e91e0fa5a367ed994a
Arthur Fernandez <arthur@snooget.com>
2023-09-12 13:46:10 -0700

    New input props to fix bug

--- app/(root)/login/page.tsx
+++ app/(root)/login/page.tsx
@@ -1,63 +1,64 @@
-"use client";
-
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
-//import OAuth from "../components/OAuth";
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  updateProfile,
-} from "firebase/auth";
-import { db } from "../../lib/firebase";
-import { doc, serverTimestamp, setDoc } from "firebase/firestore";
+import Link from "next/link";
+import OAuth from "../../components/OAuth";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { toast } from "react-toastify";
 
-export function Register() {
+export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
     email: "",
     password: "",
-    name: "",
   });
-  const { email, password, name } = formData;
-  function onChange(e: React.ChangeEvent<HTMLInputElement>) {
+  const { email, password } = formData;
+  function onChange(e) {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.id]: e.target.value,
     }));
   }
-  async function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+  async function onSubmit(e) {
     e.preventDefault();
-
     try {
       const auth = getAuth();
-      const userCredential = await createUserWithEmailAndPassword(
+      const userCredentials = await signInWithEmailAndPassword(
         auth,
         email,
         password
       );
-
-      const user = userCredential.user;
-      const formDataCopy = { ...formData };
-      delete formDataCopy.password;
-      formDataCopy.timestamp = serverTimestamp();
-
-      await setDoc(doc(db, "users", user.uid), formDataCopy);
-      toast.success("Sign up was successful");
+      if (userCredentials.user) {
+        navigate("/");
+      }
+      toast.success("Sign In Successful.");
     } catch (error) {
-      console.log(error);
-      toast.error("There was an error during registration");
+      console.log("[SIGN_IN]", error);
+      toast.error("Sign In Credentials Invalid");
     }
   }
+  useEffect(() => {
+    document.body.classList.add("page-animation");
 
+    return () => {
+      document.body.classList.remove("page-animation");
+    };
+  }, []);
   return (
     <section className="mx-auto my-14 max-w-6xl px-8 h-vh">
       <div className="my-8 max-w-[300px]  mx-auto">
         <div className="flex flex-row items-center ">
           <div className="cursor-pointer border-b-2 border-purple-600 w-full">
             <h3
-              onClick={() => router.push("/register")}
               className="uppercase font-bold text-xs text-center mb-3 tracking-wider"
+              //onClick={() => navigate("/login")}
+            >
+              login
+            </h3>
+          </div>
+          <div className="cursor-pointer border-b border-gray-400 w-full">
+            <h3
+              className="uppercase font-medium text-xs text-center mb-3 tracking-wider"
+              //onClick={() => navigate("/register")}
             >
               register
             </h3>
@@ -65,16 +66,22 @@ export function Register() {
         </div>
         <div>
           <form onSubmit={onSubmit}>
-            <div className="flex flex-col gap-3 py-6">{/* <OAuth /> */}</div>
-            <div className="flex before:border-t before:flex-1 items-center after:border-t after:flex-1"></div>
+            <div className="flex flex-col gap-3 py-6">
+              <OAuth />
+              {/* github auth  */}
+              {/* facebook auth */}
+            </div>
+            <div className="flex before:border-t before:flex-1 items-center before:border-gray-400 after:border-t after:flex-1 after:border-gray-400">
+              <p className="text-center text-xs font-medium px-3">OR</p>
+            </div>
             <div className="flex flex-col gap-3 my-6">
               <input
                 type="email"
                 id="email"
-                onChange={onChange}
                 value={email}
+                onChange={onChange}
                 className="pr-4 pl- py-3 w-full border-slate-400 border-[1px]  transition duration-150 rounded-sm text-sm bg-inherit focus:ring-purple-600 hover:border-purple-600 active:border-purple-900"
-                placeholder=" Email"
+                placeholder="Email"
               />
               <div className="bg-inherit relative">
                 <input
@@ -83,7 +90,7 @@ export function Register() {
                   value={password}
                   onChange={onChange}
                   className="pr-4 pl- py-3 w-full border-slate-400 border-[1px]  transition duration-150 rounded-sm text-sm bg-inherit focus:ring-purple-600 hover:border-purple-600 active:border-purple-900"
-                  placeholder=" Your password"
+                  placeholder="Your password"
                 />
                 {showPassword ? (
                   <AiFillEyeInvisible
@@ -97,20 +104,20 @@ export function Register() {
                   />
                 )}
               </div>
-              <input
-                type="text"
-                id="name"
-                onChange={onChange}
-                value={name}
-                className="pr-4 pl- py-3 w-full border-slate-400 border-[1px]  transition duration-150 rounded-sm text-sm bg-inherit focus:ring-purple-600 hover:border-purple-600 active:border-purple-900"
-                placeholder=" Full Name"
-              />
+              <p>
+                <Link
+                  href="/forgot-password"
+                  className="text-sm font-semibold hover:text-purple-600 transition duration-200 ease-in-out "
+                >
+                  Forgot Password?
+                </Link>
+              </p>
             </div>
             <button
               type="submit"
               className="w-full  text px-7 py-3 font-medium text-sm  rounded-sm   bg-[#6366F1] text-white hover:text-white overflow-hidden transition ease-in-out duration-150  hover:bg-[#5F56D6] active:bg-[#5B45BB] "
             >
-              Register {" >"}
+              Continue {" >"}
             </button>
           </form>
         </div>
---
+++ app/components/OAuth.tsx
@@ -0,0 +1,115 @@
+import {
+  GoogleAuthProvider,
+  TwitterAuthProvider,
+  FacebookAuthProvider,
+  getAuth,
+  signInWithPopup,
+} from "firebase/auth";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import React from "react";
+import { FcGoogle } from "react-icons/fc";
+import { FaFacebook, FaTwitter } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import { db } from "../firebase";
+import { toast } from "react-toastify";
+
+export default function OAuth() {
+  const navigate = useNavigate();
+  async function onGoogleClick() {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      //check for users
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      navigate("/account");
+    } catch (error) {
+      toast.error("Could not authorize with Google");
+    }
+  }
+  async function onFacebookClick() {
+    try {
+      const auth = getAuth();
+      const provider = new FacebookAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      //check for users
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      navigate("/account");
+    } catch (error) {
+      toast.error("Could not authorize with Facebook");
+      console.log(error);
+    }
+  }
+  async function onTwitterClick() {
+    try {
+      const auth = getAuth();
+      const provider = new TwitterAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      //check for users
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      navigate("/account");
+    } catch (error) {
+      toast.error("Could not authorize with Twitter");
+      console.log(error);
+    }
+  }
+
+  return (
+    <>
+      <button
+        type="button"
+        onClick={onGoogleClick}
+        className="flex items-center w-full hover:bg-gray-200 text-inherit pl-2 pr-4 py-3 text-sm font-semibold rounded-sm transition duration-150 border-[1px] border-slate-400 ease-in-out "
+      >
+        <FcGoogle className="bg-white rounded-full mr-2 text-2xl " />
+        Continue with Google
+      </button>
+      <button
+        onClick={onFacebookClick}
+        type="button"
+        className="flex items-center w-full hover:bg-gray-200 text-inherit pl-2 pr-4 py-3 text-sm font-semibold rounded-sm transition duration-150 border-[1px] border-slate-400 ease-in-out "
+      >
+        <FaFacebook className="bg-white text-blue-600 rounded-full mr-2 text-2xl " />
+        Continue with Facebook
+      </button>
+      <button
+        onClick={onTwitterClick}
+        type="button"
+        className="flex items-center w-full hover:bg-gray-200 text-inherit pl-2 pr-4 py-3 text-sm font-semibold rounded-sm transition duration-150 border-[1px] border-slate-400 ease-in-out "
+      >
+        <FaTwitter className="bg- text-blue-400 rounded-full mr-2 text-2xl " />
+        Continue with Twitter
+      </button>
+    </>
+  );
+}
